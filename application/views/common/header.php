4188a047936e8f70c01ae33852917f81bd421019
Sultan Mahmud <zahid.smz@live.com>
2013-07-18 17:32:20 +0600

    Theme: Added basic theme files with initial design

---
+++ application/views/common/header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">  
+    <meta http-equiv="content-type" content="text/html">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Open Mic @ Kozmo, Pandu & Jibon, event, events">  
+    <meta name="description" content="">
+    <meta name="author" content="Mangorate">
+
+    <meta property='og:title' content='Open Mic @ Kozmo' />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="http://eventconnectbd.com/events/view/397" />
+    <meta property="og:image" content="http://eventconnectbd.com/docs/photo/uploads/f241971f6844e1c6b4fde452e202cf09.jpg" />
+    <meta property="og:site_name" content="EventConnectBD" />
+    <meta property="og:description" content="Management and Pandu reserves the right to refuse any performance." />
+    <meta property="fb:admins" content="1521982625,500943320,172009206,503513862,506419912" />
+    <meta property="fb:app_id" content="448897628488531" />
+
+    <title><?php echo $title; ?></title>
+
+    <link rel="icon" type="image/png" href="<?php echo base_url(); ?>assets/img/favicon.png">
+
+    <link href='http://fonts.googleapis.com/css?family=Droid+Sans=' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/docs.css" rel="stylesheet">
+    <?php echo $css; ?>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <?php echo $js; ?>
+
+  </head>
+
+  <body>
+
+    <div class="top-area"></div> 
+         
+    <div class="top-nav-area navbar-fixed-top">
+      <div class="container">
+        <div class="span1"><img class="logo" src="assets/img/mangorate-logo.png"></div>
+        <div class="span7">
+          <form class="form-inline">
+            <div class="input-prepend">
+              <span class="add-on">Find</span>
+              <input class="input-xlarge" id="prependedInput" type="text" placeholder="biriyani, dinner, Coffee World">
+            </div>
+            <div class="input-prepend">
+              <span class="add-on">Near</span>
+              <input class="input-xlarge" id="prependedInput" type="text" placeholder="Dhanmondi, Dhaka">
+            </div>
+            <button class="btn btn-danger btn-search" type="submit"><i class="icon-search icon-white"></i></button>
+          </form> 
+        </div> 
+        <div class="span2">
+          <button class="btn btn-login btn-success" type="submit"><i class="icon-user icon-white"></i> Login</button>   
+          <button class="btn btn-signup btn-info" type="submit"><i class="icon-check icon-white"></i> Sign Up</button> 
+        </div>    
+      </div>
+      <div class="clear"></div>
+    </div>
+    
+    <div class="container">
+      <div class="navbar">
+        <div class="navbar-inner">
+          <a class="brand" href="#"><img class="text-logo" src="assets/img/mangorate-text.png"></a>
+          <ul class="nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Write a Review</a></li>
+            <li><a href="#">Find Friends</a></li>
+            <li><a href="#">Messages</a></li>
+            <li><a href="#">Community</a></li>
+            <li><a href="#">Events</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="container">
